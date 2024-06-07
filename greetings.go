4cbd985dac0cfa4b4e900b20e31ef9d4ae3cf078
ErnestoMolina <helverzon@gmail.com>
2024-06-06 23:23:04 -0500

    manejar un error

--- greetings.go
+++ greetings.go
@@ -1,11 +1,15 @@
 package greetings
 
 import (
+	"errors"
 	"fmt"
 )
 
 // Hello devuelve un saludo para la persona especifica.
-func Hello(name string) string {
+func Hello(name string) (string, error) {
+	if name == "" {
+		return "", errors.New("Nombre vacio")
+	}
 	message := fmt.Sprintf("Hola, %v! ¡Bienvenido!", name)
-	return message
+	return message, nil
 }
