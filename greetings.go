d68ddb291b601431112a29764e4856472c11cbca
ErnestoMolina <helverzon@gmail.com>
2024-06-08 20:34:48 -0500

    manejo de maps

--- greetings.go
+++ greetings.go
@@ -3,6 +3,7 @@ package greetings
 import (
 	"errors"
 	"fmt"
+	"math/rand"
 )
 
 // Hello devuelve un saludo para la persona especifica.
@@ -10,6 +11,29 @@ func Hello(name string) (string, error) {
 	if name == "" {
 		return "", errors.New("Nombre vacio")
 	}
-	message := fmt.Sprintf("Hola, %v! ¡Bienvenido!", name)
+	message := fmt.Sprintf(randonFormat(), name)
 	return message, nil
+}
+
+func Hellos(names []string) (map[string]string, error) {
+	mensages := make(map[string]string)
+	for _, name := range names {
+		mensage, err := Hello(name)
+		if err != nil {
+			return nil, err
+		}
+
+		mensages[name] = mensage
+	}
+
+	return mensages, nil
+}
+
+func randonFormat() string {
+	format := []string{
+		"Hola, %v !Bienvenido¡",
+		"!Que bueno verte¡%v",
+		"!Saludos, %v !Encantado de conocerte¡",
+	}
+	return format[rand.Intn(len(format))]
 }
